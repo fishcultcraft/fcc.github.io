0745a219681531b74f05f5e090a8f7aa97a84dc1
Cristian <defacation69@gmail.com>
2025-06-07 23:11:09 -0400

    newest lighting changes to three js

--- files/pages/main.js
+++ files/pages/main.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { Sky } from 'three/addons/objects/Sky.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true});
 renderer.outputColorSpace = THREE.SRGBColorSpace;
@@ -15,23 +17,23 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(25, 10, 11);
+camera.position.set(0, 100, 11);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.enablePan = false;
+controls.enablePan = true;
 controls.minDistance = 25; 
-controls.maxDistance = 100;
+controls.maxDistance = 200;
 controls.minPolarAngle = 0.5;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = false;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
-scene.fog = new THREE.Fog(0xffffff, 0.00000015, 250.0) //adds fog (probably gotta change this)
+scene.fog = new THREE.Fog(0xffffff, 0.00000015, 350.0) //adds fog (probably gotta change this)
 
 const sky = new Sky();
-sky.scale.setScalar(1000)
+sky.scale.setScalar(450000)
 scene.add(sky)
 
 const sun = new THREE.Vector3();
@@ -39,62 +41,121 @@ const skyUniforms = sky.material.uniforms;
 skyUniforms['turbidity'].value = 10; // Controls haziness
 skyUniforms['rayleigh'].value = 8; // Controls sky color intensity
 skyUniforms['mieCoefficient'].value = 0.08; // Controls sun halo
-skyUniforms['mieDirectionalG'].value = 0.8; // Controls sun scattering
+skyUniforms['mieDirectionalG'].value = 0.8;// Controls sun scattering
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 // Set sun position (affects sky appearance)
-const phi = THREE.MathUtils.degToRad(88); // Close to horizon for a nice effect
-const theta = THREE.MathUtils.degToRad(180); // Sun direction
+const phi = THREE.MathUtils.degToRad(60); // Close to horizon for a nice effect
+const theta = THREE.MathUtils.degToRad(90); // Sun direction
 sun.setFromSphericalCoords(1, phi, theta);
 skyUniforms['sunPosition'].value.copy(sun);
 
-const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
-groundGeometry.rotateX(-Math.PI / 2);
-const groundMaterial = new THREE.MeshStandardMaterial({
-  color: 0x555555,
-  side: THREE.DoubleSide
-});
-
-//ground
-const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-groundMesh.castShadow = false;
-groundMesh.receiveShadow = true;
-scene.add(groundMesh);
-
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.copy(sun).multiplyScalar(100)
+directionalLight.position.copy(sun).multiplyScalar(1000);
 directionalLight.castShadow = true;
-directionalLight.shadow.bias = -0.001;
-directionalLight.intensity = 5;
-directionalLight.penumbra = 1;
+directionalLight.shadow.bias = -0.0001;
+directionalLight.intensity = 10.0;
+directionalLight.penumbra = 1.0;
+directionalLight.shadow.mapSize.width = 2048;
+directionalLight.shadow.mapSize.height = 2048;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 5000;
+directionalLight.shadow.camera.left = -200;
+directionalLight.shadow.camera.right = 200;
+directionalLight.shadow.camera.top = 200;
+directionalLight.shadow.camera.bottom = -500;
 scene.add(directionalLight);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-//ambientLight.position.copy(sun)
-scene.add(ambientLight)
+// Add helper for light direction
+//const lightHelper = new THREE.DirectionalLightHelper(directionalLight, 1000);
+//scene.add(lightHelper);
+// Add helper for shadow camera frustum
+//const shadowCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+//scene.add(shadowCameraHelper);
 
-const loader = new GLTFLoader().setPath("/model/");
-loader.load('Barrel.gltf', (gltf) => {
-  console.log('loading model');
-  const mesh = gltf.scene;
-  mesh.scale.set(0.05, 0.05, 0.05) //scale
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientLight);
 
-  mesh.traverse((child) => {
-    if (child.isMesh) {
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
+
+//GLTF loader(not being used)
+//const loader = new GLTFLoader().setPath("/model/");
+//loader.load('Barrel.gltf', (gltf) => {
+//  console.log('loading model');
+//  const mesh = gltf.scene;
+//  mesh.scale.set(0.05, 0.05, 0.05); //scale
+
+//  mesh.traverse((child) => {
+//    if (child.isMesh) {
+//      child.castShadow = true;
+//      child.receiveShadow = true;
+//    }
     
-  });
+//  });
 
-  mesh.position.set(0, 0, 0); //position
-  scene.add(mesh);
+//  mesh.position.set(0, 0, 0); //position
+  //scene.add(mesh);
 
-  document.getElementById('progress-container').style.display = 'none';
-}, (xhr) => {
-  console.log(`loading ${xhr.loaded / xhr.total * 100}%`);
-}, (error) => {
-  console.error(error);
-});
+//  document.getElementById('progress-container').style.display = 'none';
+//}, (xhr) => {
+//  console.log(`loading ${xhr.loaded / xhr.total * 100}%`);
+//}, (error) => {
+//  console.error(error);
+//});
+
+// MTL loader
+const mtlLoader = new MTLLoader();
+const objLoader = new OBJLoader();
+
+document.getElementById('progress-container').style.display = 'block';
+// Load the .mtl file first
+mtlLoader.load(
+  '/model/mad/textures/Village1.mtl', // Path to your .mtl file
+  (materials) => {
+    // Preload materials
+    materials.preload();
+    // Assign materials to OBJLoader
+    objLoader.setMaterials(materials);
+
+    // Load the .obj file
+    objLoader.load(
+      '/model/mad/textures/Village1.obj', // Path to your .obj file
+      (object) => {
+        // Position the object
+        object.position.set(0, 0, 0);
+        object.scale.set(0.5,0.5,0.5)
+
+        object.traverse((child) => {
+          if (child){
+            child.castShadow = true;
+            child.receiveShadow = true;
+          }
+        })
+        // Add to scene
+        scene.add(object);
+
+        document.getElementById('progress-container').style.display = 'none';
+      },
+      (xhr) => {
+        // Progress callback
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+      },
+      (error) => {
+        // Error callback
+        console.error('An error happened loading OBJ:', error);
+      }
+    );
+  },
+  (xhr) => {
+    // Progress callback for MTL
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded (MTL)');
+  },
+  (error) => {
+    // Error callback for MTL
+    console.error('An error happened loading MTL:', error);
+  }
+);
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -108,4 +169,4 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-animate()
+animate();
